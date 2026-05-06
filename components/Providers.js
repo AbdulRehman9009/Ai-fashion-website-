d49550aa43bf7fa1617f92edc5532b7a2bc1bf5d
AbdulRehman9009 <opabdul636@gmail.com>
2026-05-06 18:10:29 +0500

    m

--- components/Providers.js
+++ components/Providers.js
@@ -6,16 +6,18 @@ import { CartProvider } from "@/contexts/CartContext";
 import PaddleLoader from "@/components/providers/PaddleLoader";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import "react-toastify/dist/ReactToastify.css";
+import NextTopLoader from "nextjs-toploader";
 
 export function Providers({ children }) {
     return (
         <SessionProvider>
             <ThemeProvider>
-                <CartProvider>
-                    <PaddleLoader />
-                    {children}
-                    <ToastContainer position="bottom-right" theme="dark" />
-                </CartProvider>
+                <NextTopLoader showSpinner={false} color={"#fff"} height={3} />
+                    <CartProvider>
+                        <PaddleLoader />
+                        {children}
+                        <ToastContainer position="bottom-right" theme="dark" />
+                    </CartProvider>
             </ThemeProvider>
         </SessionProvider>
     );
